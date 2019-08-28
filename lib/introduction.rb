2452c343750a8f6cd2f626569400df8d1487de13
ECMABERNATHY <50777925+ECMABERNATHY@users.noreply.github.com>
2019-08-28 16:10:35 -0700

    Create introduction.rb

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+#def #introduction(name="Erin")
+#  puts "Hi, my name is #{name}."
+#end
+
+#def #introduction_with_language(name="Erin", language="Ruby")
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
+
+#def #introduction_with_language_optional(name="Erin", language="Ruby")
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
+end 
